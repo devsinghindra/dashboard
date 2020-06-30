bc3ecf62f73803ac131133683d5fa8340e6455ec
Devendra Singh <devsinghindra@gmail.com>
2020-06-30 20:19:22 +0530

    added basic layout for graphs

---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC8rYPRvmeE8nuBIK8fum_BwcxYdD0KVig",
+    authDomain: "tourist-bot-hilapp.firebaseapp.com",
+    databaseURL: "https://tourist-bot-hilapp.firebaseio.com",
+    projectId: "tourist-bot-hilapp",
+    storageBucket: "tourist-bot-hilapp.appspot.com",
+    messagingSenderId: "903028130242",
+    appId: "1:903028130242:web:43780bc4bdf541d95e565b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const cloudDB = firebase.firestore();
+
+
+export default cloudDB;
